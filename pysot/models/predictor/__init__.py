ecf18f9dafae19b1084a74acce6e7d477c4aa960
Bowen Li <bowenli2@andrew.cmu.edu>
2023-10-14 10:15:32 -0400

    fixed ablation study bug

--- pysot/models/predictor/__init__.py
+++ pysot/models/predictor/__init__.py
@@ -11,16 +11,12 @@ from pysot.models.predictor.lb_5 import LearnBaseV5
 from pysot.models.predictor.lbv_5 import VisualBaseV5
 # joint predictor
 from pysot.models.predictor.mv_v16 import MVV16
-from pysot.models.predictor.mv_v16_add import MVV16Add
-from pysot.models.predictor.mv_v16_attn import MVV16Attn
 
 Predictors = {
         'KF': KalmanF,
         'LB_v5': LearnBaseV5,
         'LBv_v5':VisualBaseV5,
         'MV_v16': MVV16,
-        'MV_v16_add': MVV16Add,
-        'MV_v16_attn': MVV16Attn
        }
 
 
